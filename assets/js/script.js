539dccfadfd0c2aca375c4e9053d86fd62c1ce6e
mohamedsobhi70 <sobhi7070@gmail.com>
2023-07-04 15:00:31 +0300

    add

--- assets/js/script.js
+++ assets/js/script.js
@@ -66,7 +66,6 @@ if ($(".testimonials-carousel").length > 0) {
     $('.testimonials-carousel').owlCarousel({
         nav: false,
         dots: false,
-        loop: true,
         rtl: true,
         autoHeight: true,
         responsive: {
@@ -103,4 +102,125 @@ if ($(".mom-accordion").length) {
         $(this).find(".mom-accordion-content").slideToggle(300);
         $(this).find(".icon img").toggleClass("rotate");
     })
+}
+
+// show password input
+if ($(".show-password").length) {
+    $(".show-password").on("click", function () {
+        if ($(this).siblings("input").attr('type') == 'password') {
+            $(this).siblings("input").attr('type', 'text');
+        }
+        else {
+            $(this).siblings("input").attr('type', 'password');
+        }
+    })
+}
+
+
+// timer countdown
+if ($(".timer").length) {
+
+    $('.timer .countdown').html("02:00");
+    let interval = setInterval(function () {
+        let timer = $('.timer .countdown').html();
+        timer = timer.split(':');
+        let minutes = timer[0];
+        let seconds = timer[1];
+        seconds -= 1;
+
+        if (minutes < 0) return;
+
+        else if (seconds < 0 && minutes != 0) {
+            minutes -= 1;
+            minutes = "0" + minutes
+            seconds = 59;
+        }
+        else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
+
+        if (minutes.length < 2) minutes = '0' + minutes;
+
+        $('.timer .countdown').html(minutes + ':' + seconds);
+
+        if (minutes == 0 && seconds == 0) {
+            $('.timer .resend').removeClass("hidden");
+            $('.timer .message').addClass("hidden");
+            clearInterval(interval);
+        }
+    }, 1000);
+}
+
+
+// 6 digit confirm password
+if ($(".confirm-digit").length) {
+
+    let in1 = document.getElementById('otc-1'),
+        ins = document.querySelectorAll('.confirm-digit input'),
+        splitNumber = function (e) {
+            let data = e.data || e.target.value; // Chrome doesn't get the e.data, it's always empty, fallback to value then.
+            if (!data) return; // Shouldn't happen, just in case.
+            if (data.length === 1) return; // Here is a normal behavior, not a paste action.
+            popuNext(e.target, data);
+            //for (i = 0; i < data.length; i++ ) { ins[i].value = data[i]; }
+        },
+        popuNext = function (el, data) {
+            el.value = data[0]; // Apply first item to first input
+            data = data.substring(1); // remove the first char.
+            if (el.nextElementSibling && data.length) {
+                // Do the same with the next element and next data
+                popuNext(el.nextElementSibling, data);
+            }
+        };
+
+    ins.forEach(function (input) {
+        /**
+         * Control on keyup to catch what the user intent to do.
+         * I could have check for numeric key only here, but I didn't.
+         */
+        input.addEventListener('keyup', function (e) {
+            // Break if Shift, Tab, CMD, Option, Control.
+            if (e.keyCode === 16 || e.keyCode == 9 || e.keyCode == 224 || e.keyCode == 18 || e.keyCode == 17) {
+                return;
+            }
+
+            // On Backspace or left arrow, go to the previous field.
+            if ((e.keyCode === 8 || e.keyCode === 37) && this.previousElementSibling && this.previousElementSibling.tagName === "INPUT") {
+                this.previousElementSibling.select();
+            } else if (e.keyCode !== 8 && this.nextElementSibling) {
+                this.nextElementSibling.select();
+            }
+
+            // If the target is populated to quickly, value length can be > 1
+            if (e.target.value.length > 1) {
+                splitNumber(e);
+            }
+        });
+
+        /**
+         * Better control on Focus
+         * - don't allow focus on other field if the first one is empty
+         * - don't allow focus on field if the previous one if empty (debatable)
+         * - get the focus on the first empty field
+         */
+        input.addEventListener('focus', function (e) {
+            // If the focus element is the first one, do nothing
+            if (this === in1) return;
+
+            // If value of input 1 is empty, focus it.
+            if (in1.value == '') {
+                in1.focus();
+            }
+
+            // If value of a previous input is empty, focus it.
+            // To remove if you don't wanna force user respecting the fields order.
+            if (this.previousElementSibling.value == '') {
+                this.previousElementSibling.focus();
+            }
+        });
+    });
+
+    /**
+     * Handle copy/paste of a big number.
+     * It catches the value pasted on the first field and spread it into the inputs.
+     */
+    in1.addEventListener('input', splitNumber);
 }
